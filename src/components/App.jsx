655662c7ab027c151e1c35c80ce0ce72d4a30790
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-12-04 16:10:45 +0100

    add-repo

---
+++ src/components/App.jsx
@@ -0,0 +1,55 @@
+import { usersData } from 'data/users';
+import { Component } from 'react';
+import { Button } from './Button/Button';
+import { UsersList } from './UsersList/UsersList';
+import { Form } from './Form/Form';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    users: usersData,
+    isListShown: false,
+    isFormShown: false,
+  };
+
+  deleteUser = userId => {
+    this.setState(prevState => ({
+      users: prevState.users.filter(user => user.id !== userId),
+    }));
+  };
+
+  showUsers = () => {
+    this.setState({ isListShown: true });
+  };
+
+  openForm = () => {
+    this.setState({ isFormShown: true });
+  };
+
+  addUser = (data) => {
+    const newUser = { ...data, id: nanoid() }
+    this.setState(prevState => ({users:[...prevState.users, newUser]
+     }))
+  }
+
+  closeForm = () => {
+    this.setState({isFormShown: false})
+  }
+
+  render() {
+    const { users, isListShown, isFormShown } = this.state;
+    return (
+      <>
+        {isListShown ? (
+          <>
+            <UsersList users={users} deleteUser={this.deleteUser} />
+            <Button text="Add user" clickHandler={this.openForm} />
+          </>
+        ) : (
+          <Button text="Show users" clickHandler={this.showUsers} />
+        )}
+        {isFormShown && <Form addUser={this.addUser} closeForm = {this.closeForm} />}
+      </>
+    );
+  }
+}
